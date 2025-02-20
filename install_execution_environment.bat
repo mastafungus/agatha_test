7dc2194130802dbdf9f26c2f7eb5dea9dfdfad7b
Julien Becheny <julien.becheny@vega-logiciel.fr>
2025-02-20 08:29:59 +0100

    FIX : Mise à jour de commande d'exécution des tests

--- install_execution_environment.bat
+++ install_execution_environment.bat
@@ -1,22 +1,22 @@
 @echo off
-ECHO Installation de l'environnement d'exécution...
+ECHO Installation de l'environnement d'execution...
 
-ECHO Création de l'environnement virtuel...
+ECHO Creation de l'environnement virtuel...
 CALL python -m venv env
 
 ECHO Activation de l'environnement virtuel...
 CALL .\env\Scripts\activate
 
-ECHO Installation des dépendances...
+ECHO Installation des dependances...
 CALL pip install -r pip_requirements.txt
 
 ECHO Initialisation de la librairie Browser...
 CALL rfbrowser init
 
-ECHO Installation terminée avec succès !
+ECHO Installation terminee avec succes !
 ECHO Pour exécuter les tests, utilisez : 
-ECHO 1. .\env\Scripts\activate
-ECHO 2. robot tests/test.robot
+ECHO 1. env\Scripts\activate
+ECHO 2. robot .
 PAUSE
 
 CALL .\env\Scripts\deactivate.bat
